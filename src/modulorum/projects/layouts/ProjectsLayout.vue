5bc7f8f3e0e739af86445baac98237edfd817300
saaralozano <saloz2001@gmail.com>
2025-04-03 18:04:39 +0200

    Componentes de Daisyui

--- src/modulorum/projects/layouts/ProjectsLayout.vue
+++ src/modulorum/projects/layouts/ProjectsLayout.vue
@@ -2,7 +2,7 @@
 <template>
     <SupraMenu />
 
-    <main>
+    <main class="flex flex-row">
         <LateralisMenu />
         <router-view />
     </main>
--- src/router/index.ts
+++ src/router/index.ts
@@ -6,8 +6,16 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'projects',
-      component: ProjectsLayout
+      name: 'home',
+      redirect: {name: 'projects'},
+      component: ProjectsLayout,
+      children: [
+        {
+          path: 'projects',
+          name: 'projects',
+          component: () => import('@/modulorum/projects/views/ProjectsView.vue')
+        }
+      ]
     }
   ],
 })
