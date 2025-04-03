d9d457f096e311b7c7c0bfc99ad61203ee147943
saaralozano <saloz2001@gmail.com>
2025-04-03 17:39:33 +0200

    Creando el menú de la interface

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,14 @@
+import ProjectsLayout from '@/modulorum/projects/layouts/ProjectsLayout.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    {
+      path: '/',
+      name: 'projects',
+      component: ProjectsLayout
+    }
   ],
 })
 
