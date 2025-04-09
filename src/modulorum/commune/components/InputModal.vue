fe9002ab0c6d70f8f668f79367a0b2b1cba5b205
saaralozano <saloz2001@gmail.com>
2025-04-09 19:49:18 +0200

    Input personalizado

--- src/modulorum/commune/components/InputModal.vue
+++ src/modulorum/commune/components/InputModal.vue
@@ -1,18 +1,19 @@
 <template>
-    <dialog id="my_modal_1" class="modal" :open="true">
+    <dialog id="my_modal_1" class="modal" :open="aperta">
         <div class="modal-box">
-            <h3 class="text-lg font-bold">Hello!</h3>
-            <p class="py-4">Press ESC key or click the button below to close</p>
+            <h3 class="text-lg font-bold">{{ titulus }}</h3>
+            <p class="py-4">{{ subtitulus }}</p>
             <div class="modal-action flex flex-col">
                 <form method="dialog" @submit.prevent="submitValorem">
                     <input
+                        ref="inputRef"
                         type="text"
-                        placeholder="Nombre del proyeto"
+                        :placeholder="placeholder ?? 'Inserte un valor'"
                         class="input input-bordered w-full flex-1 input-primary"
                         v-model="inputValorem"
                     >
                     <div class="flex justify-end mt-5">
-                        <button class="btn">Close</button>
+                        <button @click="$emit('claudere')" class="btn">Close</button>
                         <button type="submit" class="btn btn-primary ml-4">Aceptar</button>
                     </div>
                 </form>
@@ -20,7 +21,7 @@
         </div>
     </dialog>
 
-    <div class="fixed top-0 w-screen h-screen z-10 bg-black opacity-40"></div>
+    <div v-if="aperta" class="fixed top-0 w-screen h-screen z-10 bg-black opacity-40"></div>
 </template>
 
 
@@ -31,18 +32,26 @@ import { ref } from 'vue';
 
 interface Props {
     aperta: boolean;
+    placeholder?: string;
+    titulus: string;
+    subtitulus: string;
 }
+
+defineProps<Props>();
 
 const emissiones = defineEmits<{
     claudere: [void];
     valorem: [text: string]
 }>()
 
-const inputValorem = ref('');
+const inputValorem = ref<string>('');
+
+const inputRef = ref<HTMLInputElement | null>(null);
 
 const submitValorem = () => {
     console.log({valorem: inputValorem.value});
     if (!inputValorem.value) {
+        inputRef.value?.focus();
         return;
     }
     emissiones('valorem', inputValorem.value.trim());
