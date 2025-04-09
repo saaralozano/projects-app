38883d9dc5324326a9e58999615e4b0cd82c17bb
saaralozano <saloz2001@gmail.com>
2025-04-09 20:27:31 +0200

    Diseño de los Modals finalizado

--- src/modulorum/projects/views/ProjectsView.vue
+++ src/modulorum/projects/views/ProjectsView.vue
@@ -32,8 +32,30 @@
     subtitulus="Dale un nombre apropiado a tu proyecto"
   />
 
+  <proprium-modal :aperta="propriumModalAperta">
+    <template #header>
+        <h1 class="text-3xl">Título del modal</h1>
+    </template>
+
+    <template #body>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto ab magni facilis odit sapiente temporibus molestias? Beatae, voluptate nulla, a quae, quia voluptatibus unde dolorum consequuntur itaque nesciunt dignissimos reiciendis?</p>
+    </template>
+
+    <template #footer>
+        <div class="flex justify-end">
+            <button @click="propriumModalAperta = false" class="btn">Close</button>
+            <button @click="propriumModalAperta = false" class="btn btn-primary ml-4">Aceptar</button>
+        </div>
+    </template>
+
+  </proprium-modal>
+
     <fab-button @click="modalAperta=true">
         <AddCircle />
+    </fab-button>
+
+    <fab-button @click="propriumModalAperta=true" positione="bottom-left">
+        <ModalIcon />
     </fab-button>
 
 
@@ -42,10 +64,15 @@
 <script lang="ts" setup>
 import FabButton from '@/modulorum/commune/components/FabButton.vue';
 import InputModal from '@/modulorum/commune/components/InputModal.vue';
+import PropriumModal from '@/modulorum/commune/components/PropriumModal.vue';
 import AddCircle from '@/modulorum/commune/icons/AddCircle.vue';
+import ModalIcon from '@/modulorum/commune/icons/ModalIcon.vue';
 import { ref } from 'vue';
 
 const modalAperta = ref(false);
+
+const propriumModalAperta = ref(false);
+
 const cumNovusValorem = (projectNomen: string) => {
     console.log(projectNomen);
 }
