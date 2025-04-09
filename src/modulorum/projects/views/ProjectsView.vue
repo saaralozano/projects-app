ee817433bc83fae677684bb31afa5f6550dbeeec
saaralozano <saloz2001@gmail.com>
2025-04-09 17:55:04 +0200

    Botón personalizado con slot

--- src/modulorum/projects/views/ProjectsView.vue
+++ src/modulorum/projects/views/ProjectsView.vue
@@ -1,34 +1,37 @@
 <template>
-
-    <div class="overflow-x-auto w-full">
+  <div class="overflow-x-auto w-full">
     <table class="table">
-        <!-- head -->
-        <thead>
+      <!-- head -->
+      <thead>
         <tr>
-            <th></th>
-            <th>Proyectos</th>
-            <th>Tareas</th>
-            <th>Avance</th>
+          <th></th>
+          <th>Proyectos</th>
+          <th>Tareas</th>
+          <th>Avance</th>
         </tr>
-        </thead>
+      </thead>
 
-        <tbody>
+      <tbody>
         <!-- row 2 -->
         <tr class="hover:bg-base-300">
-            <th>2</th>
-            <td>Hart Hagerty</td>
-            <td>Desktop Support Technician</td>
-            <td>Purple</td>
+          <th>2</th>
+          <td>Hart Hagerty</td>
+          <td>Desktop Support Technician</td>
+          <td>Purple</td>
         </tr>
-        </tbody>
+      </tbody>
     </table>
-    </div>
+  </div>
 
-    <FabButton />
+    <fab-button @click="() => console.log('clicked')">
+        <AddCircle />
+    </fab-button>
+
 
 </template>
 
 <script lang="ts" setup>
 import FabButton from '@/modulorum/commune/components/FabButton.vue';
+import AddCircle from '@/modulorum/commune/icons/AddCircle.vue';
 
 </script>
