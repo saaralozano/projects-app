950024e8b463a778bd29f614e228d58a55837d0b
saaralozano <saloz2001@gmail.com>
2025-04-11 17:15:08 +0200

    Instalación de Pinia y creación de Store

---
+++ src/modulorum/projects/stores/projects.store.ts
@@ -0,0 +1,25 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+import type { Project } from "../interfaces/project.interface";
+
+const incipiensPortat = ():Project[] => {
+    return [
+        {
+            id: '1',
+            nomen: 'Project 1',
+            chores: [],
+        },
+        {
+            id: '2',
+            nomen: 'Project 2',
+            chores: [],
+        },
+    ];
+}
+
+export const useProjectsStore = defineStore('projects', () => {
+    const projects = ref<Project[]>(incipiensPortat());
+    return {
+        projects,
+    }
+});
