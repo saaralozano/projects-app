659b370a010d9439398d1ef428066276a892e121
saaralozano <saloz2001@gmail.com>
2025-04-12 16:44:42 +0200

    Cambiamos algunas cosas

--- src/modulorum/projects/stores/projects.store.ts
+++ src/modulorum/projects/stores/projects.store.ts
@@ -19,8 +19,9 @@ export const useProjectsStore = defineStore('projects', () => {
         });
     }
     return {
-        // projects,
+        projects,
         projectList: computed(() => [...projects.value]),
         addereProject,
+        nonProject: computed(() => projects.value.length === 0),
     }
 });
